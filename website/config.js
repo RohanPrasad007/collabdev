e938f1703615a4613cd79664d3a8f2274278c81b
RohanPrasad007 <007rohanprasad@gmail.com>
2025-04-17 02:38:51 +0530

    add Avatar component for user profile images and implement thread page with real-time messaging

--- website/config.js
+++ website/config.js
@@ -1,17 +1,17 @@
 // firebase.js
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getDatabase } from 'firebase/database';
-import { getStorage } from 'firebase/storage';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyADO_LVuYvLekJw7Mmuepd1RVO4-EnF94Y',
-  authDomain: 'collabdev-b911c.firebaseapp.com',
-  projectId: 'collabdev-b911c',
-  storageBucket: 'collabdev-b911c.firebaseapp.com',
-  messagingSenderId: '176894846158',
-  appId: '1:176894846158:web:96b4edacff78edfa8c9c9f',
-  databaseURL: 'https://collabdev-b911c-default-rtdb.firebaseio.com/',
+  apiKey: "AIzaSyADO_LVuYvLekJw7Mmuepd1RVO4-EnF94Y",
+  authDomain: "collabdev-b911c.firebaseapp.com",
+  projectId: "collabdev-b911c",
+  storageBucket: "rohan-prasad-portfolio.appspot.com",
+  messagingSenderId: "176894846158",
+  appId: "1:176894846158:web:96b4edacff78edfa8c9c9f",
+  databaseURL: "https://collabdev-b911c-default-rtdb.firebaseio.com/",
 };
 
 // Initialize Firebase
@@ -20,7 +20,7 @@ const app = initializeApp(firebaseConfig);
 // Initialize Firebase services
 const auth = getAuth(app);
 const database = getDatabase(app);
-const storage = getStorage(app);
+const storage = getStorage(app, "gs://rohan-prasad-portfolio.appspot.com");
 
 // Export the app as the default export
 export default app;
